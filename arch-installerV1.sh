19631856a80123a9561b6d699db26780dfc37315
734j <assawin.h@gmail.com>
2023-02-25 22:45:59 +0100

    Disk partitioning

--- arch-installerV1.sh
+++ arch-installerV1.sh
@@ -8,3 +8,15 @@ echo
 echo
 echo w
 ) | fdisk /dev/sda
+
+mkfs.ext4 /dev/sda
+
+mount /dev/sda /mnt 
+
+echo | pacstrap -K /mnt base linux linux-firmware
+
+arch-chroot /mnt 
+
+mkinitcpio -p linux 
+
+
