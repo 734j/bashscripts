c519d260734311c32222d9391f1f6aa2b7272d35
734j <assawin.h@gmail.com>
2023-02-28 13:40:13 +0100

    didnt work

--- arch-installer-mbr.sh
+++ arch-installer-mbr.sh
@@ -16,17 +16,14 @@ mount /dev/sda /mnt
 echo | pacstrap -K /mnt base linux linux-firmware
 
 arch-chroot /mnt localectl set-locale LANG=sv_SE.UTF-8
-arch-chroot /mnt locale-gen
-
-arch-chroot /mnt (echo echo) | passwd
-
-arch-chroot /mnt useradd -m -g users -G wheel user
-arch-chroot /mnt (echo echo) | passwd user 
+arch-chroot /mnt locale-gen 
 
 arch-chroot /mnt pacman -S grub dosfstools os-prober mtools 
 arch-chroot /mnt grub-install --target=i386-pc --recheck /dev/sda 
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg 
 
 umount -a 
+
+echo "done" 
 
 
