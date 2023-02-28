adf5c0d46908b90c1461488b298227ee3a704ffa
734j <assawin.h@gmail.com>
2023-02-28 13:31:43 +0100

    i have not tested this, might not work

--- arch-installer-mbr.sh
+++ arch-installer-mbr.sh
@@ -18,10 +18,15 @@ echo | pacstrap -K /mnt base linux linux-firmware
 arch-chroot /mnt localectl set-locale LANG=sv_SE.UTF-8
 arch-chroot /mnt locale-gen
 
-arch-chroot /mnt echo | passwd
+arch-chroot /mnt (echo echo) | passwd
 
-echo "This is a test"
+arch-chroot /mnt useradd -m -g users -G wheel user
+arch-chroot /mnt (echo echo) | passwd user 
 
+arch-chroot /mnt pacman -S grub dosfstools os-prober mtools 
+arch-chroot /mnt grub-install --target=i386-pc --recheck /dev/sda 
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg 
 
+umount -a 
 
 
