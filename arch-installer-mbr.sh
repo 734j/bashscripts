a45508e547f3658ea14bbbd15ec2894c50be6e0a
734j <assawin.h@gmail.com>
2023-02-28 14:47:29 +0100

    please work2

--- arch-installer-mbr.sh
+++ arch-installer-mbr.sh
@@ -18,7 +18,7 @@ genfstab -U /mnt >> /mnt/etc/fstab
 #arch-chroot /mnt localectl set-locale LANG=sv_SE.UTF-8
 #arch-chroot /mnt locale-gen 
 
-arch-chroot /mnt echo | pacman -S grub dosfstools os-prober mtools 
+arch-chroot /mnt echo | pacman -Sy grub dosfstools os-prober mtools 
 arch-chroot /mnt grub-install --target=i386-pc --recheck /dev/sda 
 arch-chroot /mnt mkdir /boot/grub/locale 
 arch-chroot /mnt cp /usr/share/locale/en\@quor/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo 
