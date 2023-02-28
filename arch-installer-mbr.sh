f1e6d9628aa8f26bfc8f24781ddc7c28efeb60cc
734j <assawin.h@gmail.com>
2023-02-28 14:40:33 +0100

    please work

--- arch-installer-mbr.sh
+++ arch-installer-mbr.sh
@@ -2,9 +2,8 @@
 (
 echo o
 echo n 
-echo p
-echo 1
-echo
+echo 
+echo 
 echo
 echo w
 ) | fdisk /dev/sda
@@ -14,12 +13,15 @@ echo y | mkfs.ext4 /dev/sda
 mount /dev/sda /mnt 
 
 echo | pacstrap -K /mnt base linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt localectl set-locale LANG=sv_SE.UTF-8
-arch-chroot /mnt locale-gen 
+#arch-chroot /mnt localectl set-locale LANG=sv_SE.UTF-8
+#arch-chroot /mnt locale-gen 
 
-arch-chroot /mnt pacman -S grub dosfstools os-prober mtools 
+arch-chroot /mnt echo | pacman -S grub dosfstools os-prober mtools 
 arch-chroot /mnt grub-install --target=i386-pc --recheck /dev/sda 
+arch-chroot /mnt mkdir /boot/grub/locale 
+arch-chroot /mnt cp /usr/share/locale/en\@quor/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo 
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg 
 
 umount -a 
